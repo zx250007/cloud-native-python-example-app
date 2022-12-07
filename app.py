3ece553f54b769e3f101d6c04f12c1389968ba15
zx250007 <67679412+zx250007@users.noreply.github.com>
2022-12-07 23:54:08 +0100

    Update app.py
    Test changes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "This is the awesome app EVER, speaking with brad and sahdev."
+    return "Przykładowa apka"
  
 
 @app.route("/healthz")
