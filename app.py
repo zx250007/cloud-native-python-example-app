9b4c59009eaa8581040c4fd641fde8865877fd12
zx250007 <zx250007@gmail.com>
2022-12-08 01:12:13 +0100

    text change

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "Przykładowa apka 1"
+    return "Przykładowa apka python"
  
 
 @app.route("/healthz")
