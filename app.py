a5d76f17a44df6ec3fad30a805be9e8afd51629c
zx250007 <67679412+zx250007@users.noreply.github.com>
2022-12-08 12:14:57 +0100

    Update app.py
    zmiana

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "Przykładowa apka python 5"
+    return "Przykładowa apka python aktualna"
  
 
 @app.route("/healthz")
